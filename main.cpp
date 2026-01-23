f7240fcba20539f92c552586f346955eb087cdea
Brandon Daniel <bdanie38@ut.utm.edu>
2026-01-22 18:58:44 -0600

    Fourth Commit: sum 1 to n

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+int sum(int a, int b);
+
 int main() {
-  std::cout << "Hello World!" << endl;
+  std::cout << "Enter a number: " << endl;
   return 0;
+  int n;
+  std::cin >> n;
+  
+}
+sum(int a, int b)
+{
+  return ((n)*(n+1))/2;
+}
